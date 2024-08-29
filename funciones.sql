af1b7d10c981390894227e6cf60b7064edc93240
AlanAruFlores <alanaruquipa242@gmail.com>
2024-08-29 18:23:48 -0300

    funciones para cadenas

--- funciones.sql
+++ funciones.sql
@@ -67,6 +67,43 @@ select @indicePatIndex as 'PATINDEX RESULTADO';
 select str(123.456,7,2)-- en este caso le digo que el numero en decimales lo mande a string, pero que solo me muestre los 7 caracteres (2do parametro) y me muestre 2 decimales
 
 
+--len(cadena): devuelve la longitud del texto enviado como argumento
+select len('Hola SQL') as 'longitud';
+
+--char(x): retorna un caracter del codigo ASCII
+select char(100) as 'caracter';
+
+--left(cadena,longitud): retorna una parte de esa cadena especificada en la longitud, empezando desde izquierda a derecha
+select left('Hola SQL Server', 8);
+
+--right(cadena,longitud): retorna una parte de esa cadena especificada en la longitud, empezando desde derecha a izquierda
+select right('Hola SQL Server', 8);
 
 
 
+--lower(cadena) retorna la cadena en caracteres minusculas
+select lower('Base De Datos');
+
+--upper(cadena) retorna la cadena en caracteres mayusculas
+select upper('Base De Datos');
+
+
+--ltrim(cadena) elimina	la cadena con  los espacios de la izquierda eliminados
+select ltrim('   Texto    ');
+
+--rtrim(cadena) elimina	la cadena con  los espacios de la derecha eliminados
+select rtrim('     Texto    ');
+
+--replace(cadena, string_reemplazo, string_a_reemplazar): retorna la cadena con todas las ocurrencias de la subcadena reemplazo por la subcadena a reemplazar. 
+select replace('Hxla cxmx estan', 'x', 'o');
+
+--reverse(cadena): retorna la cadena invertida
+select reverse('Base de datos');
+
+
+--replicate(cadena, cantidad): concatena o repite la cadena dependiendo de su cantidad
+select replicate('Karl',4);
+
+
+--space(n): devuelve espacios n veces
+select 'Hola'+space(1)+'gente';
